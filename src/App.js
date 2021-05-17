8d505b31b67d0b4f5717e8c4dfb5ec7936274d4c
Jesse <jessedaniel@live.ca>
2021-05-17 14:20:21 -0600

    parse and render jumbotron data in container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
+import jumboData from "./fixtures/jumbo";
 import Jumbotron from "./components/jumbotron";
 
 export default function App() {
-  return <Jumbotron />;
+  return (
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <p>{item.title}</p>
+          <p>{item.subTitle}</p>
+          <p>{item.image}</p>
+          <p>{item.alt}</p>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+  );
 }
